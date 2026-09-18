2bffb3b9b55fc269c9ab87e6ada66ccb477eacdc
ponyingkua <ponyingkuapungodakedaah@gmail.com>
2026-09-18 22:27:38 +0700

    Update mtfk.py

--- mtfk.py
+++ mtfk.py
@@ -32,7 +32,7 @@ Z_LEVEL_LABEL = 6.5
 STRUCT_TEXT = "#BDBDBD"
 
 MAX_CANDLES_BY_TF = {
-    "15m": 96,   # ~24 jam
+    "15m": 48,   # ~12 jam
     "1h": 48,    # ~2 hari
     "4h": 42,    # ~7 hari
 }
