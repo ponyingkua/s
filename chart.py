ebb652d38dac8a8ccd7043dbad0afdd4972de2aa
ponyingkua <ponyingkuapungodakedaah@gmail.com>
2026-09-19 09:47:20 +0700

    Update chart.py

--- chart.py
+++ chart.py
@@ -78,9 +78,9 @@ Z_LEVEL_LABEL = 6.5
 Z_TARGET_ARROW = 7.0
 
 MAX_CANDLES_BY_TF = {
-    "15m": 48,   # ~12 jam
-    "1h": 48,    # ~2 hari
-    "4h": 42,    # ~7 hari
+    "15m": 96,   
+    "1h": 72,   
+    "4h": 48,    
 }
 
 STRUCTURE_CONTEXT = 30
